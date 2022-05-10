e0929266afc26478c0c5ba6c4ee2f7a7da067922
Vijesh Jain <vijejain@amazon.com>
2022-05-09 18:11:56 -0700

    initial commit

--- src/main/java/com/stripe/interview/Main.java
+++ src/main/java/com/stripe/interview/Main.java
@@ -3,11 +3,13 @@ package com.stripe.interview;
 import com.google.common.html.HtmlEscapers;
 
 public class Main {
-	public static void main(String... args) {
-		System.out.println("Hello world!");
-	}
+    public static void main(String... args) {
 
-	public static String useGuavaForSomeReason(String input) {
-		return HtmlEscapers.htmlEscaper().escape(input);
-	}
+        System.out.println("Hello world!");
+        System.out.println(useGuavaForSomeReason("Hey there!"));
+    }
+
+    public static String useGuavaForSomeReason(String input) {
+        return HtmlEscapers.htmlEscaper().escape(input);
+    }
 }
